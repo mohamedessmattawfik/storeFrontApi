7fb172ef742c5c9516e6bf01ecf4ce5bf93f45c2
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-02 15:54:37 +0200

    refactor: specify testing database

--- src/database.ts
+++ src/database.ts
@@ -4,12 +4,18 @@ import { Pool } from "pg";
 // Loading .env file into process.env
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_TEST_DB,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
 
 const client: Pool = new Pool({
   host: POSTGRES_HOST,
-  database: POSTGRES_DB,
+  database: ENV === "dev" ? POSTGRES_DB : POSTGRES_TEST_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
 });
