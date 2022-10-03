a8c2f259396667f206dbb931f09e898652bf53e1
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-03 10:59:58 +0200

    refactor: adding restrictoins on users and products table

--- migrations/sqls/20221002004604-products-table-up.sql
+++ migrations/sqls/20221002004604-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    price INTEGER,
-    category VARCHAR(50)
+    name VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL,
+    category VARCHAR(50) NOT NULL
 );
--- migrations/sqls/20221002004612-users-table-up.sql
+++ migrations/sqls/20221002004612-users-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firstName VARCHAR(50),
-    lastName VARCHAR(50),
-    password VARCHAR
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    password VARCHAR NOT NULL
 );
