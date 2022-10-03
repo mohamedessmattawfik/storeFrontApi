3d2fa85c0e9a41d439f8d49ff765e7cd3e25c41d
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-03 02:11:04 +0200

    feat: add product routes to routes index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./api/userRoutes";
+import productRoutes from "./api/productRoutes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/products", productRoutes);
 
 export default routes;
