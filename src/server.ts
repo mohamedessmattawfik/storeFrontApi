eeed0190c0076683ec5685fed57715a42b2fcda6
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-03 21:04:56 +0200

    feat: enable  CORS for all routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import routes from "./routes";
+import cors from "cors";
 
 const app: express.Application = express();
 const port: string = "3000";
 
 app.use(bodyParser.json());
-
+app.use(cors);
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
