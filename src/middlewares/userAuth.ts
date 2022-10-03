27ca730e7d64c472cb9effc1121e51a13926b46c
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-03 16:08:57 +0200

    feat: add middleware to identify user

---
+++ src/middlewares/userAuth.ts
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models/User";
+
+dotenv.config();
+
+const userAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const authHeader = req.headers.authorization;
+  const userID = req.params.id;
+  if (authHeader === undefined) {
+    res.status(404);
+    res.json({
+      message: "Invalid Token",
+    });
+    return;
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET!) as User;
+    if (decoded.id === userID) {
+      res.status(404);
+      res.json({
+        message: "Not Allowed",
+      });
+    }
+    next();
+  } catch (err) {
+    res.status(404);
+    res.json({
+      message: err,
+    });
+  }
+};
+
+export default userAuth;
