3ece06f5d0ad96540316d48a7b185d061e5f364e
MohamedAhmed <mohamedchoots@gmail.com>
2022-10-03 16:10:02 +0200

    feat: create enum status type for orders

--- migrations/sqls/20221002004554-orders-table-up.sql
+++ migrations/sqls/20221002004554-orders-table-up.sql
@@ -1,5 +1,6 @@
+CREATE TYPE Status AS ENUM ('completed', 'inProgress', 'closed');
 CREATE TABLE orders (
     id SERIAl PRIMARY KEY,
-    status ENUM('completed', 'inProgress') DEFAULT 'inProgress',
-    user_id BIGINT REFERENCES users(id),
+    status Status,
+    user_id BIGINT REFERENCES users(id)
 );
